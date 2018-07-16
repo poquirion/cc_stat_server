2af457c6e43bdc69f04f0bbe70713fe671f1ae5e
P-O Quirion <pioliqui@gmail.com>
2018-07-16 14:44:05 -0400

    add cors

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ app = Flask(__name__)
 
 from cc_stat_server import back
 
+
+def cors_head():
+    return {'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'OPTIONS, POST, GET, PUT, DELETE'}
+
+
 @app.route('/full')
 def reports():
-    return Response(back.reports(), mimetype="application/json")
+    return Response(back.reports(), mimetype="application/json", headers=cors_head())
 
 @app.route('/pouet')
 def pouet():
